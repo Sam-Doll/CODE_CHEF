b668b53ff9a197ca10b1f8808f20324952d9063a
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-08-17 21:18:19 +0530

    added question2.cpp

---
+++ question2.cpp
@@ -0,0 +1,19 @@
+/*Gold Coins 101
+147*/
+
+#include<iostream>
+using namespace std ;
+
+int main(){
+
+    int A , B , X , Y ;
+    cin >> A >> B >> X >> Y ;
+
+    if (X>Y){
+        cout << A ;
+    }else{
+        cout << B ;
+    }
+
+    return 0 ;
+}
