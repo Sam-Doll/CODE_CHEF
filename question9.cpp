273046acd91dd78d5ca23fdb58352043cbfec667
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-08-29 23:38:09 +0530

    added question9.cpp

---
+++ question9.cpp
@@ -0,0 +1,17 @@
+//CHRISTMAS GREETING 205
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int X;
+    cin>>X;
+    if(X==25){
+        cout<<"CHRISTMAS";
+    
+    }else{
+        cout<<"ORDINARY";
+    }
+    return 0;
+}
