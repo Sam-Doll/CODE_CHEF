2e402ec576a47d6657d688f4c6df29408a79dc0b
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-08-30 21:38:19 +0530

    added question11.cpp

---
+++ question11.cpp
@@ -0,0 +1,18 @@
+//THE IDES OF MARCH 231
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int N;
+    cin>> N;
+    if(N==15){
+        cout<< "Yes";
+        
+    }else{
+        cout<< "No";
+    }
+    return 0;
+}
+
