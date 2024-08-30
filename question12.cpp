90bef62981ea1c862d5bf4bab495a733deac3562
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-08-30 21:41:10 +0530

    added question12.cpp

---
+++ question12.cpp
@@ -0,0 +1,15 @@
+//Codechef Round 232
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int N;
+    cin>> N;
+    if(N==4){
+        cout<< "yes";
+    }else{
+        cout<<"no";
+    }
+    return 0;
+}
