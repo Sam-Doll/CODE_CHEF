da72a8316da2f596223c87c8d5989913fb58002c
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-08-31 23:41:35 +0530

    added question17.cpp

---
+++ question17.cpp
@@ -0,0 +1,16 @@
+//SUMMER TIME 242
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int X;
+    cin>>X;
+    if(X>35){
+        cout<< "YES";
+    }else{
+        cout<<"NO";
+    }
+    return 0;
+}
