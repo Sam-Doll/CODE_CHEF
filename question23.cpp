e5ce4c9502b4718bd7ad13625d4d6449e6aa9e2d
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-02 23:29:13 +0530

    added question23.cpp

---
+++ question23.cpp
@@ -0,0 +1,16 @@
+//AUDIBLE RANGE 279
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t ;
+    cin >> t;
+    while(t--){
+        int x , n ;
+        cin >> x >> n;
+        cout << (x/10)*n << endl;
+    }
+    return 0;
+}
