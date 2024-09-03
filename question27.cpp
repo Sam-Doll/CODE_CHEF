e5b5725a21f64a35879be2612668e25560d71e14
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-03 23:12:05 +0530

    added question27.cpp

---
+++ question27.cpp
@@ -0,0 +1,20 @@
+//JUST ONE MORE EPISODE 320
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t ;
+    cin >> t;
+    while(t--){
+        int x ;
+        cin >> x;
+        if(x>24){
+            cout << "yes" << endl ;
+        }else{
+            cout << "no" << endl ;
+        }
+    }
+    return 0 ;
+}
