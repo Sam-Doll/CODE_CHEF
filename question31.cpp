e13a0b36a8d78a732f1783e8d47c75751f6ad14a
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-04 23:52:56 +0530

    added question31.cpp

---
+++ question31.cpp
@@ -0,0 +1,15 @@
+// ALICE AND MARKS 362
+
+#include <iostream>
+using namespace std ;
+
+int main(){
+    int x , y ;
+    cin >> x >> y ;
+    if(x >= 2*y){
+        cout << "yes" << endl ;
+    }else{
+        cout << "no" << endl ;
+    }
+    return 0;
+}
