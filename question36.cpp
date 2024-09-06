fac0d2e5bbe9bf8f31decfb4611b3b0ebd22af38
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-06 18:38:25 +0530

    added question36.cpp

---
+++ question36.cpp
@@ -0,0 +1,20 @@
+// IS IT HOT OR COLD 410
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int c;
+        cin>>c;
+        if(c>20){
+            cout<<"hot"<<endl;
+        }else{
+            cout<<"cold"<<endl;
+        }
+    }
+    return 0;
+}
