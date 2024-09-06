8c6fea09d7d2ac58df64c255699b66c6e37ffb8f
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-06 18:39:25 +0530

    added question38.cpp

---
+++ question38.cpp
@@ -0,0 +1,19 @@
+// CHEF AND MASKS 432
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        if(x*100<y*10){
+            cout<<"disposable"<<endl;
+        }else{
+            cout<<"cloth"<<endl;
+        }
+    }
+    return 0;
+}
