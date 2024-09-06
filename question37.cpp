a8753b58cc2836f639668ba5b859f3fe5759a39c
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-06 18:39:06 +0530

    added question37.cpp

---
+++ question37.cpp
@@ -0,0 +1,17 @@
+//  FIND REMAINDER 421
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        int c=a%b;
+        cout<<c<<endl;
+    }
+    return 0;
+}
