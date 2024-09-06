8d5236b23ee9d974a4d1fa5762a0e6d45bba8c18
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-06 18:39:43 +0530

    added question39.cpp

---
+++ question39.cpp
@@ -0,0 +1,16 @@
+// AI ANALYSING CODE 445
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int c;
+    cin>>c;
+    if(c<=1000){
+        cout<<"yes"<<endl;
+    }else{
+        cout<<"no"<<endl;
+    }
+    return 0;
+}
