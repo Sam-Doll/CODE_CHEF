0d4c364910ce4637c67d978ad1c06585127e02ca
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-07 23:43:27 +0530

    added question43.cpp

---
+++ question43.cpp
@@ -0,0 +1,16 @@
+// WATER PARK 485
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int w,h;
+    cin>>w>>h;
+    if(w>=60 && h<=130){
+        cout<<"yes"<<endl;
+    }else{
+        cout<<"no"<<endl;
+    }
+    return 0;
+}
