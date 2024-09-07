73b9bc2a4e9d2243c01c04d0cb6caf5216f4e568
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-07 23:50:42 +0530

    added question44.cpp

---
+++ question44.cpp
@@ -0,0 +1,20 @@
+// BEST COUPON 496
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    float t;
+    cin>>t;
+    while(t--){
+        float x;
+        cin>>x;
+        if(x/10.0>100){
+            cout<<x/10.0<<endl;
+        }else{
+            cout<<100<<endl;
+        }
+    }
+    return 0;
+}
