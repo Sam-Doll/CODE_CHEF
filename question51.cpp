ee5e400b0d2efe4b17bfbfd779a0d73573515324
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-08 23:57:40 +0530

    added question51.cpp

---
+++ question51.cpp
@@ -0,0 +1,22 @@
+// PIZZA CUTTING 566
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n==1){
+            cout<<"yes"<<endl;
+        }else if(n%2==0){
+            cout<<"yes"<<endl;
+        }else{
+            cout<<"no"<<endl;
+        }
+    }
+    return 0;
+}
