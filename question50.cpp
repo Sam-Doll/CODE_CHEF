58c36a97f55252b6cf969787e88bdbeea1e3766d
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-08 23:53:06 +0530

    added question50.cpp

---
+++ question50.cpp
@@ -0,0 +1,20 @@
+// EXPERT SETTER 561
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y ;
+        if(y>=(x*0.5)){
+            cout<<"yes"<<endl;
+        }else{
+            cout<<"no"<<endl;
+        }
+    }
+    return 0;
+}
