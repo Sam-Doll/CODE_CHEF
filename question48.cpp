0d40789c54b1cb7980f5b6aa18218ee0ee3710ef
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-08 23:37:13 +0530

    added question48.cpp

---
+++ question48.cpp
@@ -0,0 +1,20 @@
+// YEARLY PHONE 541
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x;
+    cin>>x;
+    
+    int a;
+    a=x%10;
+    x/=10;
+    
+    int b;
+    b=x%10;
+    
+    cout<<"K"<<b<<a<<endl;
+    return 0;
+}
