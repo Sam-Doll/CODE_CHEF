ff6aaa22c893933d36199c850ec13784313bbd29
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-09 20:19:29 +0530

    added question52.cpp

---
+++ question52.cpp
@@ -0,0 +1,21 @@
+// MONOPOLY 578
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int T; 
+    cin >> T;
+    while(T--){
+        int P, Q, R, S;
+        cin >> P >> Q >> R >> S;
+
+        if(P > Q + R + S || Q > P + R + S || R > P + Q + S || S > P + Q + R) {
+            cout<<"YES"<<endl; 
+        }else{
+            cout<<"NO"<<endl; 
+        }
+    }
+    return 0;
+}
