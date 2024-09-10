a594cb6eb28ddc3f65e581ac121122323da11711
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-10 15:06:48 +0530

    added question60.cpp

---
+++ question60.cpp
@@ -0,0 +1,21 @@
+// CRICKET TOURNAMENT 669
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    
+int t;
+cin>>t;
+while(t--){
+    int n,m;
+    cin>>n>>m;
+    if(n>m){
+        cout<<"yes"<<endl;
+    }else{
+        cout<<"no"<<endl;
+    }
+}
+return 0;
+}
