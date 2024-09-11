4df086d161e48151f4ee6ba21f4cac0239beb002
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-11 22:12:02 +0530

    added question66.cpp

---
+++ question66.cpp
@@ -0,0 +1,23 @@
+// VALENTINE GIFTS 729
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t; 
+    cin>>t;
+
+    while(t--){
+        int x;  
+        cin>>x;
+        int minBudget=127;
+
+        if (x >= minBudget) {
+            cout << "YES" << endl;
+        }else{
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
