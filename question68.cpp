3388a56c9356bd85d0ae03ea9a9ee8995c1c9a01
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-12 23:34:07 +0530

    added question68.cpp

---
+++ question68.cpp
@@ -0,0 +1,18 @@
+// SUM OR DIFFERENCE 749
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n1,n2;
+    cin>>n1;
+    cin>>n2;
+    if(n1>n2){
+        cout<<n1-n2<<endl;
+    }else{
+        cout<<n1+n2<<endl;
+    }
+    
+    return 0;
+}
