3516a36a035514f828edacb6a43e22ffb14c4752
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-14 00:05:38 +0530

    added question76.cpp

---
+++ question76.cpp
@@ -0,0 +1,17 @@
+// FOOTBALL TIES
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t; 
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        int a=x%3; 
+        cout<<a<<endl;
+    }
+    return 0;
+}
