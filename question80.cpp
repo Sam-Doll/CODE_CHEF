64c0a9380e730f539f41fed561fd21756702a480
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-15 23:10:57 +0530

    added question80.cpp

---
+++ question80.cpp
@@ -0,0 +1,17 @@
+// PAPER CUTTING 800
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;  
+    cin>>t;
+    while (t--) {
+        int n,k;
+        cin>>n>>k; 
+        int m=(n/k)*(n/k);
+        cout<<m<<endl;
+    }
+    return 0;
+}
