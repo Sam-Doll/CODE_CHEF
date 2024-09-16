2beb968b848606da1de96a34c0e2e9f1ef8876df
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-16 18:23:14 +0530

    added question86.cpp

---
+++ question86.cpp
@@ -0,0 +1,16 @@
+// GUESS THE BOTTOM FACE 815
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int T;
+    cin >> T; 
+    while (T--) {
+        int X;
+        cin >> X;  
+        cout << 7 - X << endl;
+    }
+    return 0;
+}
