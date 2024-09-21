12e5577c6f6042682dafc3ce942737b6787f900f
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-21 20:39:39 +0530

    added question103.cpp

---
+++ question103.cpp
@@ -0,0 +1,16 @@
+// VALID PAIR 851
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a==b||b==c||a==c){
+        cout<<"yes"<<endl;
+    }else{
+        cout<<"no"<<endl;
+    }
+    return 0;
+}
