759fe6311a859922bd8546bf445faf71a47a2d59
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-22 23:58:29 +0530

    added question107.cpp

---
+++ question107.cpp
@@ -0,0 +1,16 @@
+// COLDPLAY 855
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while (t--) {
+        int M, S;
+        cin >> M >> S;
+        cout << M / S << endl;
+    }
+    return 0;
+}
