6074181305dc8fce9e35e4893e2efc2c224ebf2c
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-26 23:56:16 +0530

    added question115.cpp

---
+++ question115.cpp
@@ -0,0 +1,22 @@
+// WHICH DIVISION 867
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+     int T;
+    cin >> T;
+    while (T--) {
+        int R;
+        cin >> R;
+        if (R >= 2000) {
+            cout << 1 << endl;
+        } else if (R >= 1600) {
+            cout << 2 << endl;
+        } else {
+            cout << 3 << endl;
+        }
+    }
+    return 0;
+}
