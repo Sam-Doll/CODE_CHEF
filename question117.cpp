de02ae46032e82419eebfb97a6643b33060a97e1
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-26 23:56:53 +0530

    added question117.cpp

---
+++ question117.cpp
@@ -0,0 +1,16 @@
+// RELATIVITY 872
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int T;
+    cin >> T;
+    while (T--) {
+        int g, c;
+        cin >> g >> c;
+        cout << (c * c) / (2 * g) << endl;
+    }
+    return 0;
+}
