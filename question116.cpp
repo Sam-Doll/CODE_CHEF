76050c188b4b09fc482f38c60d922e0ec7eceb4a
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-26 23:56:29 +0530

    added question116.cpp

---
+++ question116.cpp
@@ -0,0 +1,16 @@
+// BADMINTON SERVES 869
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+     int T;
+    cin >> T;
+    while (T--) {
+        int P;
+        cin >> P;
+        cout << (P / 2) + 1 << endl;
+    }
+    return 0;
+}
