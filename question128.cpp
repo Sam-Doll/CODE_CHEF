9b1bb9fb66edf9f8abdd10b58ae387e50489a526
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-29 23:49:20 +0530

    added question128.cpp

---
+++ question128.cpp
@@ -0,0 +1,16 @@
+// CHEF AND PROFITS 889
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y,z;
+        cin>>x>>y>>z;
+        cout<<(z*x)-(y*x)<<endl;
+    }
+    return 0;
+}
