91f4959350a8150403e828bcd8ce817065a841ad
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-09-30 17:19:40 +0530

    added question133.cpp

---
+++ question133.cpp
@@ -0,0 +1,16 @@
+// HIDDEN NUMBERS 917
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout << "1 " << n << endl;
+    }
+    return 0;
+}
