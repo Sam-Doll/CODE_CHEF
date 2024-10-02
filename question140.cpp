a51032777b3d4db61b4712a2946fa6fd5db1ccfe
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-10-02 22:59:52 +0530

    added question140.cpp

---
+++ question140.cpp
@@ -0,0 +1,20 @@
+// ADD 1 OR 2 GAME 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n==1){
+            cout<<"alice"<<endl;
+        }else{
+            cout<<"bob"<<endl;
+        }
+    }
+    return 0;
+}
