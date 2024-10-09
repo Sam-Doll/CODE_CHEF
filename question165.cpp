3d058aaf5d3262a4d688e997b016cea48a03df38
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-10-09 22:31:08 +0530

    added question165.cpp

---
+++ question165.cpp
@@ -0,0 +1,16 @@
+// chef and parole : s-155
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x;
+    cin>>x;
+    if(x>=7){
+        cout<<"yes"<<endl;
+    }else{
+        cout<<"no"<<endl;
+    }
+    return 0;
+}
