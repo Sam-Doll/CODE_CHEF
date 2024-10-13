2b0232bff3e6c35fb005c44477e45251ae87196a
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-10-13 22:59:15 +0530

    added question166.cpp

---
+++ question166.cpp
@@ -0,0 +1,33 @@
+// EVEN-TUAL REDUCTION 1040
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+        string S;
+        cin >> S;
+        
+        int freq[26] = {0};
+        for (char c : S) {
+            freq[c - 'a']++;
+        }
+
+        bool possible = true;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] % 2 != 0) { 
+                possible = false;
+                break;
+            }
+        }
+
+        if (possible) cout << "YES\n";
+        else cout << "NO\n";
+    }
+    return 0;
+}
