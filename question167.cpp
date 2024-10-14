f563d87d81654e15bbaf94b49df53703ac8568c9
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-10-14 23:57:00 +0530

    added question167.cpp

---
+++ question167.cpp
@@ -0,0 +1,24 @@
+// Airline Restrictions 1042
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    
+    while (T--) {
+        int A, B, C, D, E;
+        cin >> A >> B >> C >> D >> E;
+        
+        if ((A + B <= D && C <= E) || 
+            (A + C <= D && B <= E) || 
+            (B + C <= D && A <= E)) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+    
+    return 0;
+}
